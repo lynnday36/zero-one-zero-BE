c9ddd7f6b541c9f7d839595a2dc0066e5134eb0a
Nino <lynnday3366@naver.com>
2023-09-10 18:58:01 +0900

    feature:기본 투표테이블 정보 리스트 반환

--- zero_one_zero/src/main/java/com/example/zero_one_zero/dto/VoteValuesDto.java
+++ zero_one_zero/src/main/java/com/example/zero_one_zero/dto/VoteValuesDto.java
@@ -1,17 +1,2 @@
-package com.example.zero_one_zero.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@ToString
-
-public class VoteValuesDto {
-    private Long voteValueId;
-    private Long roomId;
-    private String voteLabel;
+package com.example.zero_one_zero.dto;public class VoteValuesDto {
 }
---
+++ zero_one_zero/src/main/java/com/example/zero_one_zero/dto/VotingroomDto.java
@@ -0,0 +1,2 @@
+package com.example.zero_one_zero.dto;public class VotingroomDto {
+}

8b6ae545c7b19838c93de0b05f3ec32ca5922965
Nino <lynnday3366@naver.com>
2023-09-11 04:04:41 +0900

    fix : API 이름 변경

--- zero_one_zero/src/main/java/com/example/zero_one_zero/controller/VotingroomController.java
+++ zero_one_zero/src/main/java/com/example/zero_one_zero/controller/VotingroomController.java
@@ -12,17 +12,16 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vote")
 public class VotingroomController {
     @Autowired
     private VotingroomService votingroomService;
 
-    @GetMapping("/{roomCode}") //룸코드들어왔을때 룸아이디페이지로 리다이렉션
+    @GetMapping("/vote/{roomCode}") //룸코드들어왔을때 룸아이디페이지로 리다이렉션
     public ResponseEntity<?> getVote(@PathVariable String roomCode) {
         Long roomId = votingroomService.findRoomIdByCode(roomCode);
         HttpHeaders headers = new HttpHeaders();
         if (roomId != null) {
-            headers.setLocation(URI.create("/vote/room/"+ roomId));
+            headers.setLocation(URI.create("/room/"+ roomId));
             return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
         } else {
             headers.setLocation(URI.create("/vote"));
@@ -32,19 +31,20 @@ public class VotingroomController {
     /*
      *
      *일단 룸아이디로 해놨는데 룸코드들어오면 리다이렉트 걸거나 해야함->리다이렉트해결*/
-    @GetMapping("/room/{roomId}")
+    @GetMapping("/vote/room/{roomId}")
     public VotingroomDto getVoteAsId(@PathVariable Long roomId){ //실질적으로 정보 보여주는 메서드
 
         return votingroomService.getVotingroomDto(roomId);
 
     }
 
-    //투표 생성, 생성과 동시에 수정코드 반환
-    @PutMapping("/create")
+    //투표 생성, 생성과 동시에 입장코드 반환
+    @PostMapping("/putCreateNewVote")
     public String putCreateNewVote(@RequestBody createVoteDto requestDto) { //이름입력받을때 크리에이터 네임 따로 받아야함
         String roomCode = votingroomService.createVotingroom(requestDto);
         return roomCode;
     }
+
 
 
 }

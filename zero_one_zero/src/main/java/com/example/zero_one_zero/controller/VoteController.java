d8c32a4a4725c2d6922d9561604aa06912cef8d4
Nino <lynnday3366@naver.com>
2023-09-23 01:30:11 +0900

    perf : 투표 결과 api 분리/putCastVote 리턴값 수정

--- zero_one_zero/src/main/java/com/example/zero_one_zero/controller/VoteController.java
+++ zero_one_zero/src/main/java/com/example/zero_one_zero/controller/VoteController.java
@@ -3,9 +3,7 @@ package com.example.zero_one_zero.controller;
 import com.example.zero_one_zero.dto.*;
 import com.example.zero_one_zero.exceptions.ResourceNotFoundException;
 import com.example.zero_one_zero.service.VotingService;
-import com.example.zero_one_zero.service.VotingroomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,19 +21,27 @@ public class VoteController {
         votingService.updateParticipantSelection(roomId,username.getUserName());
     }*/
 
-    @PutMapping("/room/{roomId}/putCastVote") //투표 수행 -> 성공/fail 여부, 이때 selectedname이랑 votevalueid 모두 변경
-    public ResponseEntity<VoteStatisticsDto> putCastVote(@PathVariable Long roomId, @RequestBody doVoteDto castVoteInfo){//투표실행 - votevalueId, userName가 들어옴
-        HttpHeaders headers = new HttpHeaders();
+    @PutMapping("/room/{roomId}/putCastVote") //투표 수행 -> 성공/fail 여부 리턴, 이때 selectedname이랑 votevalueid 모두 변경
+    public ResponseEntity<String> putCastVote(@PathVariable Long roomId, @RequestBody doVoteDto castVoteInfo){//투표실행 - votevalueId, userName가 들어옴
         try{
         votingService.executeVote(roomId, castVoteInfo.getUserName(), castVoteInfo.getVoteValueId());
-        VoteStatisticsDto voteStatistics = votingService.calculateVoteResults(roomId);
+            return ResponseEntity.ok("vote success");
+        } catch(ResourceNotFoundException er){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @GetMapping("/room/{roomId}/voteResults") //투표결과 api
+    public ResponseEntity<VoteStatisticsDto> getVoteResults(@PathVariable Long roomId){
+        try{
+            VoteStatisticsDto voteStatistics = votingService.calculateVoteResults(roomId);
             return new ResponseEntity<>(voteStatistics, HttpStatus.OK);
         } catch(ResourceNotFoundException er){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } //이건 성공/fail 여부만 하고 투표 결과 리턴하는 api를 따로 빼기
+        }
     }
 
-    @PutMapping("/room/{roomId}/finishVote")
+    @PutMapping("/room/{roomId}/finishVote") //투표끝, 벌칙자 이름 api
     public ResponseEntity<finishVoteResponseDto> finishVote(@PathVariable Long roomId, @RequestBody finishVoteRequestDto requestDto) {
         finishVoteResponseDto responseDto = votingService.finishVote(roomId, requestDto.getModifyCode());
 
